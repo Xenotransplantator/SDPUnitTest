591787300d10f63591f7769f9be1aec0b305b420
Piotr Sawala <56836342+Xenotransplantator@users.noreply.github.com>
2022-02-13 21:01:23 +0100

    Add files via upload

---
+++ MathLibrary/PrimeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    namespace PrimeNumbers
+    {
+        public static class PrimeNumber
+        {
+            public static bool IsPrime(int number)
+            {
+                if (number < 0)
+                {
+                    throw new ArgumentException("Liczba nie moze byc ujemna !!");
+                }
+
+                if (number == 0 || number == 1)
+                {
+                    return false;
+                }
+
+                for (int i = 2; i <= number / 2; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
---
+++ MathLibraryNUnitTest/PrimeNumberTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AutoFixture;
+using FluentAssertions;
+using MathLibrary.PrimeNumbers;
+using Moq;
+using NUnit.Framework;
+
+namespace MathLibraryUnitTest
+{
+    public class PrimeNumberTest
+    {
+        [Test]
+        public void IsPrime_NegativeValue_Exception()
+        {
+            Assert.Throws<ArgumentException>(() => PrimeNumber.IsPrime(-5));
+        }
+
+        [TestCase(0, false)]
+        [TestCase(1, false)]
+        [TestCase(2, true)]
+        [TestCase(3, true)]
+        [TestCase(4, false)]
+        [TestCase(5, true)]
+        [TestCase(6, false)]
+        public void IsPrime_IntegerValue_ReturnsIsPrime(int value, bool expectedValue)
+        {
+            // act
+            var actualValue = PrimeNumber.IsPrime(value);
+
+            // assert
+            actualValue.Should().Be(expectedValue);
+        }
+    }
+}
